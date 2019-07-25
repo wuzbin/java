3cbc6a786162b7e5bc521cc9c79f9207f3250a16
吴张斌 <wuzhangbin@meituan.com>
2019-07-25 19:54:26 +0800

    删除空行

--- src/main/java/com/test/internalapi/InternalApiTest.java
+++ src/main/java/com/test/internalapi/InternalApiTest.java
@@ -18,9 +18,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  * To change this template use File | Settings | File Templates.
  */
 public class InternalApiTest {
-
     private static List<Long> times = new ArrayList<Long>();
-
     private static SortedMap<Long, Integer> statis = new TreeMap<Long, Integer>();
 
     public static void main(String[] args){
